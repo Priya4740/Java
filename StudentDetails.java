8b47b3a5676bb49caa650d4ea2a9f8eb167d1be1
Priya4740 <152766706+Priya4740@users.noreply.github.com>
2024-10-15 15:24:53 +0530

    Create StudentDetails.java

---
+++ StudentDetails.java
@@ -0,0 +1,13 @@
+public class MyClass {
+  public static void main(String args[]) {
+   // int x=10;
+   // int y=25;
+   // int z=x+y;
+
+    System.out.println("\t\t\t\tStudentDetails\t\t");
+    System.out.println("Name:M.priyanka\t\t\t\t Regdno:23K61A4740");
+    System.out.println("Fathername:Srinuvasarao\t\t Adharno:438099087769\t");
+    System.out.println("Occupation:Farmer\t\t\t D.O.B:21/4/2005");
+    System.out.println("Parent mobileno:8096288093\t No.of subjects passed:10");
+  }
+}
