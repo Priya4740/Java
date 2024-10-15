9908e01afb1ea5bfe42ca5fed550ed96c7bc4d6a
Priya4740 <152766706+Priya4740@users.noreply.github.com>
2024-10-15 15:24:00 +0530

    Create ArthimeticOperations.java

---
+++ ArthimeticOperations.java
@@ -0,0 +1,14 @@
+public class MyClass {
+  public static void main(String args[]) {
+    int x=10;
+    float y=25;
+    float z=x+y;
+    System.out.println("Sum of "+x+" and "+y+ " is " + z);
+    System.out.println("Difference of "+x+" and "+y+ " is " +(x-y));
+    System.out.println("Multiplication of "+x+" and "+y+ " is " +(x*y));
+    System.out.println("Division of "+x+" and "+y+" is " +(x/y));
+    System.out.println("Modulus of "+x+" and "+y+" is " +(x%y));
+    System.out.println("Square root of "+x+ "is "+Math.sqrt(x));
+    System.out.println("Square root of"+y+ "is "+Math.sqrt(y));
+  }
+}
