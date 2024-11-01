a9ba76482358f117b66b38bf822e38e73fbb4b85
Priya4740 <152766706+Priya4740@users.noreply.github.com>
2024-11-01 10:18:13 +0530

    Create strings.java

---
+++ create strings.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+public class Main {
+    public static void main(String args[]) {
+        String s1="Welcome";
+        char ch[]={'P','R','I','Y','A'};
+        String s2=new String(ch);
+        String s3=new String("Example program");
+        System.out.println("The first string is "+s1);
+        System.out.println("The second string is "+s2);
+        System.out.println("The third string is "+s3);
+    }
+}
