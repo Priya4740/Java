615e34075ee478e8a3a1229eb65a5b6ff2381a3f
Priya4740 <152766706+Priya4740@users.noreply.github.com>
2024-11-01 10:11:43 +0530

    Create stringconcatenation.java

---
+++ stringconcatenation.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.lang.*;
+public class Main{
+    public static void main(String args[]) {
+        String s1 = "priyanka";
+        s1.concat("Maruboina");
+        System.out.println("String s1 is: "+s1);
+        String s2 = "priyanka";
+        String s3 = s2.concat("Maruboina");
+        System.out.println("String s3 is: "+s3);
+    }
+}
